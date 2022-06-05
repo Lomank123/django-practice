eb3ae340f9e65d3edb0ad99a7fdd3ed2c5ae3388
Lomank <lomank20022@gmail.com>
2022-06-05 16:47:30 +0500

    Add blog models
    - Also add blog app to apps list in the settings

--- main/blog/models.py
+++ main/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=120, verbose_name="Title")
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name="Author")
+    text = models.TextField(max_length=2000, verbose_name="Text")
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name="Creation date")
+
+
+class BlogComment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name="User")
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, verbose_name="Post")
+    text = models.CharField(max_length=400, verbose_name="Text")
--- main/main/settings/settings.py
+++ main/main/settings/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # Our apps
     'accounts',
     'testapp',
+    'blog',
 ]
 
 MIDDLEWARE = [
